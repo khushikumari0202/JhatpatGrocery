1fc3ad5e86707890b7cdee0ddae1d72d57961e0c
khushikumari0202 <kk2682117@gmail.com>
2025-05-12 19:07:18 +0530

    Updates for today

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,11 +4,14 @@ import { useNavigate } from "react-router-dom";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({children}) =>{
-    const navigate= useNavigate();
+    const navigate = useNavigate();
     const [user, setUser] = useState(null)
     const [isSeller, setIsSeller] = useState(false)
+    const [showUserLogin, setshowUserLogin] = useState(false)
 
-    const value = {navigate, user, setUser, setIsSeller, isSeller}
+    const value = {navigate, user, setUser, setIsSeller, isSeller, 
+        showUserLogin, setshowUserLogin
+    }
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>

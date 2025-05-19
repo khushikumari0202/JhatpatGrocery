b07d7382e0111bf48422837ea46793266fdd2af9
khushikumari0202 <kk2682117@gmail.com>
2025-05-19 13:44:15 +0530

    updates

---
+++ client/src/pages/ProductCategory.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useAppContext } from '../context/AppContext'
+import { useParams } from 'react-router-dom'
+import { categories } from '../assets/assets'
+
+const ProductCategory = () => {
+
+    const { products } = useAppContext()
+    const {category} = useParams()
+
+    const searchCategory = categories.find((item)=> item.path.toLowerCase() === category)
+
+    const filteredProducts = products.filter((product)=> product.category.toLowerCase() === category)
+  return (
+    <div>
+       {searchCategory && (
+        <div>
+            <p>{searchCategory.text.toUpperCase()}</p>
+        </div>
+       )}
+    </div>
+  )
+}
+
+export default ProductCategory
